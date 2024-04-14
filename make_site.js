527ea23618b2c854ca81780143bc5741f6950f23
Eamonn O'Brien-Strain <eobrain@gmail.com>
2024-04-14 14:34:45 -0700

    Add template rendering

--- make_site.js
+++ make_site.js
@@ -1,6 +1,7 @@
 import fs from 'node:fs/promises'
 import showdown from 'showdown'
 import { mkdirIfNecessary } from './file.js'
+import Mustache from 'mustache'
 
 const converter = new showdown.Converter()
 
@@ -10,9 +11,12 @@ const postMetadata = JSON.parse(await fs.readFile('post_metadata.json'))
 const siteDir = '_site'
 await mkdirIfNecessary(siteDir)
 
+const postTemplate = await fs.readFile('layout/post.html', 'utf8')
+
 // Convert markdown to HTML and write to site directory
-for (const { slug, id } of postMetadata) {
+for (const { slug, id, title, created } of postMetadata) {
   const markdown = await fs.readFile(`markdown/${slug}-${id}.md`, 'utf8')
-  const html = converter.makeHtml(markdown)
-  await fs.writeFile(`${siteDir}/${slug}-${id}.html`, html)
+  const contentHtml = converter.makeHtml(markdown)
+  const rendered = Mustache.render(postTemplate, { contentHtml, slug, title, created })
+  await fs.writeFile(`${siteDir}/${slug}-${id}.html`, rendered)
 }
