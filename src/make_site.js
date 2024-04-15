8fb51b8f73168efa72885dcf30bae1954696a094
Eamonn O'Brien-Strain <eobrain@gmail.com>
2024-04-14 17:20:07 -0700

    Render pages as well as posts

--- src/make_site.js
+++ src/make_site.js
@@ -5,7 +5,8 @@ import Mustache from 'mustache'
 
 const converter = new showdown.Converter()
 
-// Read post metadata from JSON file
+// Read metadata from JSON files
+const pageMetadata = JSON.parse(await fs.readFile('content/page_metadata.json'))
 const postMetadata = JSON.parse(await fs.readFile('content/post_metadata.json'))
 
 const siteDir = '_site'
@@ -19,8 +20,12 @@ async function render (slug, template, title, created) {
 }
 
 const postTemplate = await fs.readFile('layout/post.html', 'utf8')
+const pageTemplate = postTemplate // TODO: Create a separate layout for pages
 
 // Convert markdown to HTML and write to site directory
+for (const { slug, title } of pageMetadata) {
+  await render(slug, pageTemplate, { slug, title })
+}
 for (const { slug, title, created } of postMetadata) {
   await render(slug, postTemplate, { slug, title, created })
 }
